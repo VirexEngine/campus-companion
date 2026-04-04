a873e55b9e7ab1a28f84fe6cbeb87e0501e3cd32
VirexEngine <pratyushhapii@gmail.com>
2026-04-05 04:29:54 +0530

    updated CORS for Vercel deployment

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,13 @@ import google.generativeai as genai
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'super-secret-key-change-in-prod'
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://campus-companion-pi.vercel.app"
+        ]
+    }
+})
 jwt = JWTManager(app)
 
 @app.route("/")
